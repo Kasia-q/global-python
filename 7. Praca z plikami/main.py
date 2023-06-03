ef9a0322f097fd2b4e51fc088b7a29edbd268de3
Kasia-q <kasia21bak@gmail.com>
2023-06-03 21:45:43 +0200

    changed code so the result fits one posted in chat

--- 7. Praca z plikami/main.py
+++ 7. Praca z plikami/main.py
@@ -4,32 +4,36 @@ dir_path = path.dirname(__file__)
 nazwa_pliku = 'tekst.txt'
 data_path = path.join(dir_path, nazwa_pliku)
 
+
+if not path.exists(data_path):
+    exit()
+
 with open(data_path, 'r', encoding='utf-8') as plik:
     lines = plik.readlines()
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "u", "p", "r", "s", "t", "u", "w", "y", "z", "x"]
-    result = 0
+    plik.close()
 
-    x = lines[0].split()
-    y = lines[2].split()
-    z = lines[4].split()
-    lista_slow = x + y + z
+    slowa = []
+    for line in lines:
+        slowa += line.split()
     
-    for i in range(0, len(lista_slow)-1, +1):
-        lista_slow[i].split()
-        if lista_slow[i].endswith(",") or lista_slow[i].endswith("."):
-            lista_slow[i].split()
-            lista_slow[i] = ''.join(lista_slow[i][:-1])
-    print(lista_slow)
-    print(f"Liczba słów w tym tekście: {len(lista_slow)}")
+    for i in range(len(slowa)):
+        if "." in slowa[i]:
+            slowa[i] = slowa[i].replace(".", "")
+        if "," in slowa[i]:
+            slowa[i] = slowa[i].replace(",", "")
+        if ";" in slowa[i]:
+            slowa[i] = slowa[i].replace(";", "")
+
+    print(f"Liczba słów w tym tekście: {len(slowa)}")
     
-    for i in alphabet:
-        for j in lista_slow:
-            if j.endswith(i):
-                result = result + 1
-            else:
-                continue
-        if result > 0:
-            print(f"Liczba wyrazów kończąca się na literę {i}: {result}")
-            result = 0
-        else:
-            result = 0
+    konc_litera = []
+    for slowo in slowa:
+        litera = slowo[-1]
+        if litera not in konc_litera and litera.isalpha():
+            konc_litera.append(litera)
+            
+    stats = {}
+    for litera in konc_litera:
+        stats[litera] = sum(slowo.endswith((litera)) for slowo in slowa)
+        
+    print(stats) 
